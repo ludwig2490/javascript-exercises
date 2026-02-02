32c001fc351157df7bf2657d1d433d16eb37a046
Clodoaldo Ausec Ludwig <clodoaldo.ludwig@gmail.com>
2026-02-01 23:15:57 -0300

    Odin project exercises done until 01/02/2026

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num === 0) {
+        string = '';
+    } else if (num < 0) {
+        string = 'ERROR';
+    } else {
+        let str = string;
+        for (i = 1; i < num; i++) {
+            string = string + str;
+        }
+    }
+    return string;
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArray = string.split('');
+    let strLength = string.length;
+    let strReversed = [];
+    for (i = strLength - 1; i >= 0; i--) {
+        strReversed.push(strArray[i]);
+    }
+    return strReversed.join('');
 };
 
 // Do not edit below this line
